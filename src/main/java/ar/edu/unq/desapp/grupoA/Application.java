c6cafb80bf9f59ee961c5fc013db9a54dd85aeb3
Nahuel Benitez <beniteznahueloscar@gmail.com>
2019-08-25 20:52:59 -0300

    Modified package structure

--- src/main/java/ar/edu/unq/desapp/grupoA/Application.java
+++ src/main/java/ar/edu/unq/desapp/grupoA/Application.java
@@ -1,14 +1,16 @@
+package ar.edu.unq.desapp.grupoA;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
-@EnableAutoConfiguration
+@RestController
+@SpringBootApplication
 public class Application {
 
     @GetMapping
-    String home() {
+    public String home(){
         return "Hello World!";
     }
 
