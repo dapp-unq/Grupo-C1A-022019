31dc7c7ee47695f2c869e5b9c25110d213990be1
Nahuel Benitez <beniteznahueloscar@gmail.com>
2019-12-08 01:16:39 -0300

    Removed unnecesary annotation

--- src/main/java/ar/edu/unq/desapp/grupoa/Config.java
+++ src/main/java/ar/edu/unq/desapp/grupoa/Config.java
@@ -5,11 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class Config implements WebMvcConfigurer {
     @Bean
     public RestTemplate restTemplate() {
