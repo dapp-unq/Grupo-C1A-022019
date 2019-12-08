6f3dcb6c03aee31339a2bf3246a20643d2777466
Nahuel Benitez <beniteznahueloscar@gmail.com>
2019-12-08 20:35:14 -0300

    Removed commented code

--- src/main/java/ar/edu/unq/desapp/grupoa/Config.java
+++ src/main/java/ar/edu/unq/desapp/grupoa/Config.java
@@ -4,11 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class Config/* implements WebMvcConfigurer*/ {
+public class Config {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
@@ -23,9 +21,4 @@ public class Config/* implements WebMvcConfigurer*/ {
         loggingFilter.setMaxPayloadLength(64000);
         return loggingFilter;
     }
-
-/*    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/rest/**");
-    }*/
 }
